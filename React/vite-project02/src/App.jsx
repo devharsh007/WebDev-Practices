24f308d6b2f8dee104f58ab6253874973f08e3a7
HARSH SHARMA <69196581+devharsh007@users.noreply.github.com>
2023-11-04 20:11:42 +0530

    Adding Vite project

---
+++ React/vite-project02/src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from './assets/Sidebar'
+import SignForm from './assets/SignForm'
+function App() {
+
+  return (
+    <>
+      {/* <Sidebar /> */}
+      <SignForm />
+    </>
+  )
+}
+
+export default App
